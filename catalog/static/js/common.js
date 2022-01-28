3c119c65d046cc1331458e202b80ffb1b8d9643c
Dmitriy Ignatenko <titarx@gmail.com>
2022-01-28 13:42:21 +0400

    Создание и иподключение шаблонов. Статические файлы. Подготовка и вывод информации на главной страницы.

---
+++ catalog/static/js/common.js
@@ -0,0 +1,5 @@
+'use strict'
+
+window.addEventListener('load', function (event) {
+    console.log('Проверка');
+});
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def index(request):
-    return HttpResponse('Главная страница проекта WorldBooks')
+    # Получение количества записей
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_authors = Author.objects.all().count()
+
+    # Количество доступных книг
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors
+    }
+
+    return render(request, 'catalog/front.html', context=context)
