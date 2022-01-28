bebe7c863ba56fd84d5e09f863c0505084077d33
Dmitriy Ignatenko <titarx@gmail.com>
2022-01-28 11:58:42 +0400

    Работа с админкой, изменение отображения моделей в админке.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Author)  # Декоратор @register
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)  # Декоратор @register
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'language', 'display_author')
+    list_filter = ('genre', 'author')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)  # Декоратор @register
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('book', 'status')
+    fieldsets = (
+        (
+            'Экземпляры книг',
+            {
+                'fields': ('book', 'imprint', 'inv_nom')
+            }
+        ),
+        (
+            'Статус и окончание его действия',
+            {
+                'fields': ('status', 'due_back')
+            }
+        )
+    )
+
+
+# admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Book, BookAdmin)
+# admin.site.register(BookInstance, BookInstanceAdmin)
+admin.site.register(Status)
+admin.site.register(Genre)
+admin.site.register(Language)
--- catalog/models.py
+++ catalog/models.py
@@ -42,6 +42,11 @@ class Book(models.Model):
         # Возвращает URL-адрес для доступа к определённому экземпляру книги
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_author(self):
+        return ', '.join([author.last_name for author in self.author.all()])
+
+    display_author.short_description = 'Авторы'
+
 
 class Status(models.Model):
     name = models.CharField(max_length=20, help_text='Введите статус экземпляра книги', verbose_name='Статус экземпляра книги')
